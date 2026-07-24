eabb6f6ed6b8404af2ea6eb01687cc74a51f0c4f
mishrashivam13 <shivammishrasm597364@gmail.com>
2026-07-24 11:03:29 +0530

    new code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -153,7 +153,7 @@ export default function Hero({ onOpenContact }) {
           >
             <source src={herovideo} type="video/mp4" />
           </video>
-          <div className="absolute inset-0 bg-gradient-to-b from-[#0B0B0E]/80 via-[#0B0B0E]/60 to-[#0B0B0E]/90" />
+          <div className="absolute inset-0 bg-gradient-to-b from-[#0B0B0E]/75 via-[#0B0B0E]/55 to-[#0B0B0E]/85" />
         </div>
 
         <div className="max-w-4xl mx-auto px-5 sm:px-6 lg:px-8 relative z-10 text-center gse-body">
